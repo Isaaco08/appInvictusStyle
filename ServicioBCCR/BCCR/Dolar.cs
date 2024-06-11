5fa045550e59b21a4adebff375c6fed8b009a903
Isaac Alvarado <134655309+Isaaco08@users.noreply.github.com>
2024-06-11 15:50:27 -0600

    Servicio BCCR agregado

---
+++ ServicioBCCR/BCCR/Dolar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioBCCR
+{
+    public class Dolar
+    {
+        public string Codigo { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public double Monto { get; set; }
+
+    }
+}
